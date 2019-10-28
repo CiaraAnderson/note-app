97e65716295b1d241c79e7771cae06878776c1e6
Ciara Anderson <thelaylinguist@gmail.com>
2019-10-28 19:41:15 +0000

    Add functionality to Edit button

--- src/App.js
+++ src/App.js
@@ -28,6 +28,10 @@ getNotes = () => {
   .catch((err) => console.log(err.response.data) );
 }
 
+getNote = () => {
+  console.log('Clicked!');
+}
+
 render() {
    const { showNote, notes } = this.state;
 
@@ -41,6 +45,7 @@ render() {
         <List
           getNotes={this.getNotes}
           notes={notes}
+          getNote={this.getNote}
         />
       }
     </div>
--- src/components/List.js
+++ src/components/List.js
@@ -8,7 +8,8 @@ class List extends Component {
   }
 
   render() {
-     const { notes } = this.props;
+     const { notes, getNote } = this.props;
+
       
       const cards = notes.map((note, index) => {
       return (
@@ -16,10 +17,11 @@ class List extends Component {
           key={index}
           index={index}
           note={note}
+          getNote={getNote}
         />
       );
     });
-      
+
     return (
       <div className="list-container">
         {cards}
